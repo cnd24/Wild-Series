e8ea18e6f5f68d34ab5b4d426e87cf8687094763
Cindy Planchenault <cnd.planchenault@gmail.com>
2019-12-16 11:04:02 +0100

    liens connexion et deconnexion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Il y existe déjà un compte avec cet email")
  */
 class User implements UserInterface
 {
